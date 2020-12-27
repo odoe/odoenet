6cdeae1f50e653d1ea4326f9183019c9ce939fac
odoe <odoe@odoe.net>
2020-12-27 10:21:49 -0800

    compress images

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a conversion of [odoe.net](https://odoe.net) to a static site using [dojo](https://dojo.io).
 
 This is still a work in progress, and much of the markdown still needs some cleanup for code snippets.
+
+The `/images` folder contains all the original images before compression.
--- scripts/webp.js
+++ scripts/webp.js
@@ -1,24 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-const imagemin = require("imagemin");
-const imageminWebp = require("imagemin-webp");
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminGiflossy = require('imagemin-giflossy');
 
 (async () => {
-    // const files = await fs.promises.readdir('./assets/uses');
-    // for (let file of files) {
-    //     console.log(file)
-    //     try {
-    //         await imagemin([`./assets/uses/${file}/images/*.{jpg,png}`], {
-    //             destination: `./assets/uses/${file}/images/`,
-    //             plugins: [imageminWebp()]
-    //         });
-    //         console.log('Images converted successfully');
-    //     } catch(err) {
-    //         // do nothing
-    //     }
-    // }
-    await imagemin([`./assets/made/images/*.{jpg,png}`], {
-        destination: `./assets/made/images/`,
-        plugins: [imageminWebp()]
-    });
+    const files = await fs.promises.readdir('./assets/blog');
+    for (let file of files) {
+        console.log(file)
+        try {
+            await imagemin([`./assets/blog/${file}/images/*.{jpg,png,gif}`], {
+                destination: `./assets/blog/${file}/images/`,
+                plugins: [
+                    // imageminMozjpeg({quality: 50}),
+                    // imageminPngquant({quality: [0.3, 0.5]}),
+                    // imageminGiflossy({lossy: 60}),
+                    imageminWebp()
+                ]
+            });
+            console.log('Images converted successfully');
+        } catch(err) {
+            // do nothing
+        }
+    }
+    // await imagemin([`./assets/uses/images/*.{jpg,png,gif}`], {
+    //     destination: `./assets/uses/images/`,
+    //     plugins: [
+    //         imageminMozjpeg({quality: 50}),
+    //         imageminPngquant({quality: [0.3, 0.5]}),
+    //         imageminGiflossy({lossy: 80}),
+    //         imageminWebp()
+    //     ]
+    // });
 })();
