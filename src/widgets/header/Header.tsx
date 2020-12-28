171eb0cad74a3a4a83ab707cca117fdafc26bc09
Rene Rubalcava <rrubalcava@esri.com>
2020-12-27 17:38:05 -0800

    small lint

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,5 +1,5 @@
 import { tsx, create } from '@dojo/framework/core/vdom';
-import Link from '@dojo/framework/routing/Link'
+import Link from '@dojo/framework/routing/Link';
 import * as css from './Header.m.css';
 
 const factory = create().properties<{ title: string }>();
@@ -8,7 +8,6 @@ export default factory(({ properties }) => {
 	const { title } = properties() || 'My Site';
 	return (
 		<header classes={[css.root]}>
-			
 			<div classes={[css.title]}>
 				<Link to="/" isOutlet={false} classes={[css.link]}>
 					{title}
