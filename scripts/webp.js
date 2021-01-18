db6204215ffd1b856e047ae160aabaa22b40e4b3
odoe <odoe@odoe.net>
2021-01-18 09:29:25 -0800

    add jsapi-node blog, add md code css

--- scripts/webp.js
+++ scripts/webp.js
@@ -25,8 +25,8 @@ const imageminGiflossy = require('imagemin-giflossy');
     //         // do nothing
     //     }
     // }
-    await imagemin([`./assets/blog/webpack-module-federation/images/*.{jpg,png,gif}`], {
-        destination: `./assets/blog/webpack-module-federation/images/`,
+    await imagemin([`./assets/blog/jsapi-node/images/*.{jpg,png,gif}`], {
+        destination: `./assets/blog/jsapi-node/images/`,
         plugins: [
             imageminMozjpeg({quality: 50}),
             imageminPngquant({quality: [0.3, 0.5]}),
