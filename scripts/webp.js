e345fb37122d740992ed25b2e8d2762a6e54cbcc
Rene Rubalcava <rrubalcava@esri.com>
2022-04-07 15:11:26 -0700

    add react portals popup blog

--- scripts/webp.js
+++ scripts/webp.js
@@ -26,7 +26,7 @@ const imageminGiflossy = require('imagemin-giflossy');
 	//     }
 	// }
 
-	const blog = `unit-testing-jsapi`;
+	const blog = `popup-react-portals`;
 
 	await imagemin([`./assets/blog/${blog}/images/*.{jpg,png,gif}`], {
 		destination: `./assets/blog/${blog}/images/`,
