3a2c21db9ff0aad7e9dd824f3a54e5495b8083a4
Rene Rubalcava <rrubalcava@esri.com>
2022-05-22 14:17:00 -0700

    reactiveutils post

--- scripts/iframe-to-md.js
+++ scripts/iframe-to-md.js
@@ -6,21 +6,18 @@ const path = './markdown-pages/blog';
 // const path = './posts_bak';
 
 (async () => {
-    const files = await fs.promises.readdir(path);
-    for (let file of files) {
-        try {
-            const md = await fs.promises.readFile(`${path}/${file}/index.md`, 'utf-8');
-            if (md.includes('<script src="https://gist')) {
-                // const mdUpdate = md.replace(/<iframe\s+.*?\s+src=("(.*?)").*?<\/iframe>/gi, '!($2)');
-                // const mdUpdate = md.replace(/\[gist\s+id=(.*?)\]/gi, '[gist](https://gist.github.com/odoe/$1)');
-                const mdUpdate = md.replace(/<script.*?src="(.*?)".*?<\/script>/gi, '[gist]($1)');
-                await fs.promises.writeFile(`${path}/${file}/index.md`, mdUpdate);
-            }
-            console.log('updated successfully');
-        } catch(err) {
-            // do nothing
-            console.log('oops: ', err.message);
-        }
-    }
-
+	const files = await fs.promises.readdir(path);
+	for (let file of files) {
+		try {
+			const md = await fs.promises.readFile(`${path}/${file}/index.md`, 'utf-8');
+			if (md.includes('<script src="https://gist')) {
+				// const mdUpdate = md.replace(/<iframe\s+.*?\s+src=("(.*?)").*?<\/iframe>/gi, '!($2)');
+				// const mdUpdate = md.replace(/\[gist\s+id=(.*?)\]/gi, '[gist](https://gist.github.com/odoe/$1)');
+				const mdUpdate = md.replace(/<script.*?src="(.*?)".*?<\/script>/gi, '[gist]($1)');
+				await fs.promises.writeFile(`${path}/${file}/index.md`, mdUpdate);
+			}
+		} catch (err) {
+			// do nothing
+		}
+	}
 })();
--- scripts/webp.js
+++ scripts/webp.js
@@ -26,7 +26,7 @@ const imageminGiflossy = require('imagemin-giflossy');
 	//     }
 	// }
 
-	const blog = `solidjs-jsapi`;
+	const blog = `intro-reactiveutils`;
 
 	await imagemin([`./assets/blog/${blog}/images/*.{jpg,png,gif}`], {
 		destination: `./assets/blog/${blog}/images/`,
