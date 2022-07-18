7cbe53eeb3f9df00883427aa9b6eed57b4ad9637
Rene Rubalcava <rrubalcava@esri.com>
2022-07-17 19:51:18 -0700

    feat: hitTest blog post
    Adding new blog post on the hitTes

--- scripts/webp.js
+++ scripts/webp.js
@@ -26,7 +26,7 @@ const imageminGiflossy = require('imagemin-giflossy');
 	//     }
 	// }
 
-	const blog = `view-padding`;
+	const blog = `hittest-return`;
 
 	await imagemin([`./assets/blog/${blog}/images/*.{jpg,png,gif}`], {
 		destination: `./assets/blog/${blog}/images/`,
