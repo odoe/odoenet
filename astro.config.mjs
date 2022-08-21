6b2c929b8eef77d006c3c43bc0839818bf46ae9f
odoe <odoe@odoe.net>
2022-08-21 14:42:03 -0700

    chore: working on home page and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: "./output/dist",
   site: "https://odoe.net",
-  integrations: [sitemap()],
+  integrations: [image(), sitemap()],
 });
