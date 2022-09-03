d1223fcb71f3cb2ccd9eee66dc6718b1305175e1
Rene Rubalcava <rrubalcava@esri.com>
2022-09-02 17:24:57 -0700

    feat: add toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,19 @@ import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
+import toc from "@jsdevtools/rehype-toc";
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: "./output/dist",
-  site: "https://odoe.net",
-  integrations: [sitemap()],
-  markdown: {
-    rehypePlugins: [
-      [rehypeExternalLinks, { target: "_blank", rel: "nofollow" }],
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: "before" }],
-    ],
-  },
+	outDir: "./output/dist",
+	site: "https://odoe.net",
+	integrations: [sitemap()],
+	markdown: {
+		rehypePlugins: [
+			[rehypeExternalLinks, { target: "_blank", rel: "nofollow" }],
+			rehypeSlug,
+			[rehypeAutolinkHeadings, { behavior: "before" }],
+			toc
+		],
+	},
 });
